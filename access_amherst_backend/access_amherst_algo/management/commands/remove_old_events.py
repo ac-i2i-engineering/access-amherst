ffc4e6013dd67c35b62194f16addf9644fca09ef
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2024-11-11 20:18:19 -0500

    Update remove_old_events.py with event logger for the ones that are deleted

--- access_amherst_backend/access_amherst_algo/management/commands/remove_old_events.py
+++ access_amherst_backend/access_amherst_algo/management/commands/remove_old_events.py
@@ -1,16 +1,22 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from access_amherst_algo.models import Event
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    help = "Deletes events that have a start_time older than 24 hours"
+    help = "Deletes events that have a start_time older than 2 hours"
 
     def handle(self, *args, **kwargs):
         threshold_time = timezone.now() - timezone.timedelta(hours=2)
-        deleted_count, _ = Event.objects.filter(
-            start_time__lt=threshold_time
-        ).delete()
+        old_events = Event.objects.filter(start_time__lt=threshold_time)
+        
+        deleted_count, _ = old_events.delete()
+        
+        for event in old_events:
+            logger.info(f"Deleted event: ID={event.id}, Name={event.name}, Start Time={event.start_time}, Link={event.link}")
+
         self.stdout.write(
             self.style.SUCCESS(f"Deleted {deleted_count} old event(s).")
         )
